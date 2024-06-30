cbc272348888aee6b6e7faf06d1e40bb54842573
shah18122 <smit_shah18122@yahoo.com>
2024-07-01 04:26:28 +0530

    number guessing game

---
+++ Stopwatch_worldclock_timer.py
@@ -0,0 +1,15 @@
+import time
+
+print("Welcome to Countdown Timer!")
+
+#seconds = int(input("Enter the number of seconds to count down: "))
+'''
+for i in range(seconds, 0, -1):
+    print(i)
+    time.sleep(1)
+print("Time's up!")
+'''
+
+for i in range(0, 10000000000000000000000000000000000000, 1):
+    print(i)
+    time.sleep(1)
